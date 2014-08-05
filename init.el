44238b8c1704361f0dc918f75e4927e95ec4dcfc
Timothy Chen <tnachen@gmail.com>
2014-08-05 15:13:59 -0700

    Use 2 space indent for protobuf

--- init.el
+++ init.el
@@ -158,6 +158,13 @@
 ;;protobuf mode
 (require 'protobuf-mode)
 
+(defconst my-protobuf-style
+  '((c-basic-offset . 2)
+    (indent-tabs-mode . nil)))
+
+(add-hook 'protobuf-mode-hook
+  (lambda () (c-add-style "my-style" my-protobuf-style t)))
+
 ;; git-modes
 (add-to-list 'load-path "~/.emacs.d/lisp/git-modes/")
 ;; magit
